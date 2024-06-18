6b89c3a8ce127812b4ca534c8bc8f52562e1c68f
betowrm <marqueswr@outlook.com>
2024-06-17 21:27:13 -0400

    modificado para ver o teste

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -46,10 +46,10 @@
                                     </svg>
                                 </div>
 
-                                <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">Documentation</h2>
+                                <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">vai par ao inferno</h2>
 
                                 <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
-                                    Laravel alteraeo par ager o comitt. Whether you are a newcomer or have prior experience with Laravel, we recommend reading our documentation from beginning to end.
+                                    pita que par9u
                                 </p>
                             </div>
 
